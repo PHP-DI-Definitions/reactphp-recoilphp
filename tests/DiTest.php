3f2582c6063ebf4d80a77ebae865ef9d550c1887
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-10-28 23:18:20 +0100

    Basic unit test

---
+++ tests/DiTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+use ApiClients\Tools\TestUtilities\TestCase;
+use Psr\Log\LoggerInterface;
+use React\EventLoop\LoopInterface;
+use Recoil\Kernel;
+use Recoil\React\ReactKernel;
+
+final class DiTest extends TestCase
+{
+    public function testKernel()
+    {
+        $loop = $this->prophesize(LoopInterface::class);
+        $logger = $this->prophesize(LoggerInterface::class);
+
+        $root = dirname(__DIR__);
+        $path = $root . DIRECTORY_SEPARATOR . 'etc' . DIRECTORY_SEPARATOR . 'di' . DIRECTORY_SEPARATOR . 'kernel.php';
+        $kernel = (require $path)[Kernel::class]($loop->reveal(), $logger->reveal());
+
+        self::assertInstanceOf(Kernel::class, $kernel);
+        self::assertInstanceOf(ReactKernel::class, $kernel);
+    }
+}
